0a3976a890248e8693732490403fe914c739899b
Drik Qlis <drikqlis@gmail.com>
2023-06-04 15:37:29 +0200

    create venv

---
+++ helpers/create_venv.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Specify the Python version you want to use (optional)
+PYTHON_VERSION="3.11"
+
+# Get the parent directory of the script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+
+# Specify the name for your virtual environment
+VENV_NAME="venv"
+
+# Create the virtual environment
+python$PYTHON_VERSION -m venv "$PARENT_DIR/$VENV_NAME"
+
+# Create the virtual environment
+python$PYTHON_VERSION -m venv "$VENV_NAME"
+
+# Activate the virtual environment
+source "$VENV_NAME/bin/activate"
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install pip requirements from the parent directory
+pip install -r "$PARENT_DIR/requirements.txt"
+
+# Print success message
+echo "Python virtual environment '$VENV_NAME' has been created and activated."
